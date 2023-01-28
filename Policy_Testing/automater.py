c1cd28ba206bd88f9a53e3c106c6be228a274dd6
y0uf0ol <82054892+y0uf0ol@users.noreply.github.com>
2023-01-28 16:39:36 +0100

    first write to azure

---
+++ Policy_Testing/automater.py
@@ -0,0 +1,40 @@
+import json
+import requests
+from msgraph.core import GraphClient
+import msgraph.core
+from azure.identity import ClientSecretCredential
+
+users_url = 'https://graph.microsoft.com/v1.0/users'
+ca_url = 'https://graph.microsoft.com/v1.0/identity/conditionalAccess/policies'
+test_ca_url = 'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/testCA.json'
+
+# Create a ServicePrincipalCredentials object
+credentials = ClientSecretCredential(
+    client_id='ff20e021-d49f-4532-ab17-47dbf72181e4',
+    client_secret='cNX8Q~SuxVeAD.y9gveP6IIlVX8jCY2tsDbTdbKv',
+    tenant_id='e0c0089e-139c-46e7-a82f-231cd621849e'
+)
+
+# Create a Graph client
+graph_client = GraphClient(credential=credentials)
+#client = GraphClient(user)
+
+# Get the list of users
+#users_read = graph_client.get(users_url)
+#print(users_read.json())
+
+
+#Get the list of policies
+policies = graph_client.get(ca_url)
+
+#for policy in policies.json()['value']:
+  #  print(policy['displayName'])
+
+#Create a new policy
+re = requests.get(test_ca_url).json()
+new_policy = graph_client.post(ca_url, json=re)
+
+
+
+
+
