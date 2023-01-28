9f0dd22d726e792f8ad1fe0c2377ddf6187bc339
y0uf0ol <82054892+y0uf0ol@users.noreply.github.com>
2023-01-28 17:37:51 +0100

    v0.1

--- Policy_Testing/automater.py
+++ Policy_Testing/automater.py
@@ -6,7 +6,8 @@ from azure.identity import ClientSecretCredential
 
 users_url = 'https://graph.microsoft.com/v1.0/users'
 ca_url = 'https://graph.microsoft.com/v1.0/identity/conditionalAccess/policies'
-test_ca_url = ['https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/testCA.json', 'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/testCA2.json', 'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/testCA3.json']
+test_ca_url = ['https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/testCA.json', 'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/testCA2.json', 'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/testCA3.json']
+base_url = 'https://github.com/y0uf0ol/MS_CA_Automater/blob/main/Policy_Testing/Base%20Protection/Links.md' # Base Protection Policies
 
 # Create a ServicePrincipalCredentials object
 credentials = ClientSecretCredential(
@@ -14,20 +15,17 @@ credentials = ClientSecretCredential(
     client_secret='cNX8Q~SuxVeAD.y9gveP6IIlVX8jCY2tsDbTdbKv',
     tenant_id='e0c0089e-139c-46e7-a82f-231cd621849e'
 )
-
 # Create a Graph client
 graph_client = GraphClient(credential=credentials)
-#client = GraphClient(user)
-# Get the list of users
-#users_read = graph_client.get(users_url)
-#print(users_read.json())
-#Get the list of policies
-#policies = graph_client.get(ca_url)
-#for policy in policies.json()['value']:
-  #  print(policy['displayName'])
+
+
+# Collect all Base Protection Policies
+print('Collecting Base Protection Policies')
+
+
 
 #Create a new policy
+print('Creating new policy')
 for cap in test_ca_url:
     re = requests.get(cap).json()
-    print(re)
     new_policy = graph_client.post(ca_url, json=re)
