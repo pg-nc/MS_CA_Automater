5a0bd3c9bcdb1d6411f5f98f641d9cfffa8df6ed
y0uf0ol <82054892+y0uf0ol@users.noreply.github.com>
2023-02-06 11:33:14 +0100

    test

--- Policy_Testing/automater.py
+++ Policy_Testing/automater.py
@@ -7,14 +7,14 @@ from azure.identity import ClientSecretCredential
 users_url = 'https://graph.microsoft.com/v1.0/users'
 ca_url = 'https://graph.microsoft.com/v1.0/identity/conditionalAccess/policies'
 grp_url = 'https://graph.microsoft.com/v1.0/groups'
-test_ca_url = ['https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/testCA.json', 'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/testCA2.json', 'https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/testCA3.json']
+test_ca_url = ['https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA506_AttackSurfaceReduction_SeviceAccounts_AllApps_AllPlatforms_UntrustedLocation_BlockAccess.json','https://raw.githubusercontent.com/y0uf0ol/MS_CA_Automater/main/Policy_Testing/Base%20Protection/CA502_AttackSurfaceReduction_Admins_AllApps_UnknownDevicePlatforms_BlockAccess.json']
 base_url = 'https://github.com/y0uf0ol/MS_CA_Automater/blob/main/Policy_Testing/Base%20Protection/Links.md' # Base Protection Policies
 
 # Create a ServicePrincipalCredentials object
 credentials = ClientSecretCredential(
-    client_id='ff20e021-d49f-4532-ab17-47dbf72181e4',
-    client_secret='cNX8Q~SuxVeAD.y9gveP6IIlVX8jCY2tsDbTdbKv',
-    tenant_id='e0c0089e-139c-46e7-a82f-231cd621849e'
+    client_id='2acf62c4-cee1-4c55-b2fd-f10d4f734327',
+    client_secret='Dda8Q~kvPNMPfWBBdnAWk6oTZR56Vi2MGVzpva5g',
+    tenant_id='1376e5cb-6a36-4804-beba-0078c7b1329d'
 )
 # Create a Graph client
 graph_client = GraphClient(credential=credentials)
